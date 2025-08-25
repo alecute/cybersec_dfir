a33cc743531f3ed160c746a7c8596c96ff16b746
alecute <johnalec.pawelas@pfda.gov.ph>
2025-08-25 15:43:50 +0800

    Add files via upload

---
+++ Untitled3.ps1
@@ -0,0 +1,10 @@
+﻿implementition data Confidentialy
+using Public Keys:
+@coreBABA;
+config t
+ip domain-name pfda.gov.ph
+crypto key generate rsa
+!!tanong, anong keysize: 256.512, 1024
+ip ssh version 2
+ip ssh time-out 120
+end
